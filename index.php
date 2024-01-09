630c19d6114c6cd03ead329831d12fb58c8f1ec4
olaormundos <vanderleidasilvaormundo@gmail.com>
2024-01-09 00:20:57 -0300

    marcação

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body <?php body_class(); ?>>
+
+    <div id="page" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="searchbox">
+                    Search
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="nav-menu">
+                    Menu
+                </nav>
+            </section>
+        </header>
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <section class="hero">
+                        Slider
+                    </section>
+                    <section class="services">
+                        Service
+                    </section>
+                    <section class="home-blog">
+                        Blog
+                    </section>
+                </main>
+            </div>
+        </div>
+        <footer class="site-footer">
+            Footer
+        </footer>
+    </div>
+    
+</body>
+</html>
