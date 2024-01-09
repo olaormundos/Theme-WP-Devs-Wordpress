b4e1bc1f63fbcea186c9df3e9fa4e5451fb976b3
olaormundos <vanderleidasilvaormundo@gmail.com>
2024-01-09 00:37:37 -0300

    criação dos blocos simples footer, header index

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body <?php body_class(); ?>>
+
+    <div id="page" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="searchbox">
+                    Search
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="nav-menu">
+                    Menu
+                </nav>
+            </section>
+        </header>
--- index.php
+++ index.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body <?php body_class(); ?>>
-
-    <div id="page" class="site">
-        <header>
-            <section class="top-bar">
-                <div class="logo">
-                    Logo
-                </div>
-                <div class="searchbox">
-                    Search
-                </div>
-            </section>
-            <section class="menu-area">
-                <nav class="nav-menu">
-                    Menu
-                </nav>
-            </section>
-        </header>
+<?php get_header(); ?>
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
@@ -37,10 +14,4 @@
                 </main>
             </div>
         </div>
-        <footer class="site-footer">
-            Footer
-        </footer>
-    </div>
-    
-</body>
-</html>
+<?php get_footer(); ?>
