6e55bfd5a52c3927eac4453da95b9e8b492137ed
olaormundos <vanderleidasilvaormundo@gmail.com>
2024-01-11 10:37:37 -0300

    Adicionar arquivo da fonte Google e style css

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
     function wpdevs_load_scripts() {
-        wp_enqueue_style( 'wpdevs-style', get_stylesheet_uri(), array(), '1.0', 'all' );
+        wp_enqueue_style( 'wpdevs-style', get_stylesheet_uri(), array(), filemtime( get_template_directory(). '/style.css' ), 'all' );
+        wp_enqueue_style( 'wpdevs-google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array(), null );
     }
     add_action( 'wp_enqueue_scripts', 'wpdevs_load_scripts' );
--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
