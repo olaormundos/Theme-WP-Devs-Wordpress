78ccdc0f061c0c59dba84ab5bfd176aed6b3b162
olaormundos <vanderleidasilvaormundo@gmail.com>
2024-01-15 21:50:48 -0300

    loop simples dos posts blog

--- index.php
+++ index.php
@@ -9,7 +9,32 @@
                         Service
                     </section>
                     <section class="home-blog">
-                        Blog
+                        <div class="container">
+                            <div class="blog-items">
+                                <?php
+                                if( have_posts() ):
+                                    while( have_posts() ): the_post();
+                                    ?>
+
+                                        <article>
+                                            <h2>
+                                                <?php the_title(); ?>
+                                            </h2>
+                                            <div class="meta-info">
+                                                <p>Posted in <?= get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                                                <p>Categories: <?php the_category( ' ' ); ?></p>
+                                                <p>Tags: <?php the_tags( '', ', ' ); ?></p>
+                                            </div>
+                                            <?php the_content(); ?>
+                                        </article>
+
+                                    <?php
+                                    endwhile;
+                                else: ?>
+                                    <p>Nothing yet to be displayed!</p>
+                                <?php endif; ?>
+                            </div>
+                        </div>
                     </section>
                 </main>
             </div>
