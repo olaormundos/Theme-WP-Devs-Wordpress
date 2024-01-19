d8fd892ba456c75f507cb35c08311f26be592755
olaormundos <vanderleidasilvaormundo@gmail.com>
2024-01-18 23:55:43 -0300

    area de widget na seção servicos

--- templates/home-template.php
+++ templates/home-template.php
@@ -12,7 +12,30 @@ Template Name: Home
                         Slider
                     </section>
                     <section class="services">
-                        Service
+                        <h2>Services</h2>
+                        <div class="container">
+                            <div class="service-item">
+                                <?php 
+                                    if(is_active_sidebar( 'services-1' )){
+                                        dynamic_sidebar( 'services-1' );
+                                    }
+                                ?>
+                            </div>
+                            <div class="service-item">
+                                <?php 
+                                   if(is_active_sidebar( 'services-2' )){
+                                        dynamic_sidebar( 'services-2' );
+                                    }
+                                ?>
+                            </div>
+                            <div class="service-item">
+                                <?php 
+                                    if(is_active_sidebar( 'services-3' )){
+                                        dynamic_sidebar( 'services-3' );
+                                    }
+                                ?>
+                            </div>
+                        </div>
                     </section>
                     <section class="home-blog">
                         <div class="container">
