8c5dbb670f64512b2889607f459d2217472a0587
olaormundos <vanderleidasilvaormundo@gmail.com>
2024-01-22 21:26:13 -0300

    criação do single e loop do post

--- index.php
+++ index.php
@@ -15,15 +15,19 @@
 
                                 <article>
                                     <h2>
-                                        <?php the_title(); ?>
+                                        <a href="<?php the_permalink(); ?>">
+                                            <?php the_title(); ?>
+                                        </a>    
                                     </h2>
-                                    <?php the_post_thumbnail('thumbnail'); ?>
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php the_post_thumbnail('thumbnail'); ?>
+                                    </a>    
                                     <div class="meta-info">
                                         <p>Posted in <?= get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
                                         <p>Categories: <?php the_category( ' ' ); ?></p>
                                         <p>Tags: <?php the_tags( '', ', ' ); ?></p>
                                     </div>
-                                    <?php the_content(); ?>
+                                    <?php the_excerpt(); ?>
                                 </article>
 
                             <?php
--- templates/home-template.php
+++ templates/home-template.php
@@ -51,8 +51,14 @@ Template Name: Home
                                 while( $postlist->have_posts() ): $postlist->the_post();
                                 ?> 
                                     <article class="latest-news">
-                                        <?php the_post_thumbnail( 'large' ); ?>
-                                        <h3> <?php the_title(); ?> </h3>
+                                        <a href="<?php the_permalink(); ?>">
+                                            <?php the_post_thumbnail( 'large' ); ?>
+                                        </a>    
+                                        <h3>
+                                            <a href="<?php the_permalink(); ?>">
+                                                <?php the_title(); ?> 
+                                            </a>
+                                        </h3>
                                         <p>
                                             <div class="meta-info">
                                                 by <span><?php the_author_posts_link(); ?></span>
