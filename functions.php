f5c00215e22bfcdea2faeb4b9608c93fce23a87b
olaormundos <vanderleidasilvaormundo@gmail.com>
2024-01-25 22:41:45 -0300

    Theme customizer - criando seção de copyright

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+    require get_template_directory() . '/inc/customizer.php';
+
     function wpdevs_load_scripts() {
         wp_enqueue_style( 'wpdevs-style', get_stylesheet_uri(), array(), filemtime( get_template_directory(). '/style.css' ), 'all' );
         wp_enqueue_style( 'wpdevs-google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array(), null );
